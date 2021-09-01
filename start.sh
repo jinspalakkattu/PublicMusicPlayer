9e8daa1a2ffdb8607dde2e6571bec226c6b0b310
Jins Mathew <jins.palakkattu@gmail.com>
2021-09-01 12:27:03 +0300

    Initial Commit

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/subinps/MusicPlayer.git /MusicPlayer
-cd /MusicPlayer
+git clone https://github.com/jinspalakkattu/PublicMusicPlayer.git /PublicMusicPlayer
+cd /PublicMusicPlayer
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
